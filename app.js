72e8b5fbb538662f0a86b3cdf4df062bf72c8a94
Kevin <KevinAndHayley@kevins-imac.home>
2020-06-10 15:37:03 +0100

    Updated homepage controller and view

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ const catalogRouter = require('./routes/catalog');
 var app = express();
 
 // connect to mongoDB
-var mongoDB = 'mongodb+srv://kevin_beall:Sophia1@cluster0-nnezr.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = 'mongodb://kevin_beall:Sophia1@cluster0-shard-00-00-nnezr.mongodb.net:27017,cluster0-shard-00-01-nnezr.mongodb.net:27017,cluster0-shard-00-02-nnezr.mongodb.net:27017/local_library?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 
-mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
@@ -28,8 +28,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', catalogRouter);
+// app.use('/users', usersRouter);
 app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
